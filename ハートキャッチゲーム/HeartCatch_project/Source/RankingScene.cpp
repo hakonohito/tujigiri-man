6ed970adc9402af18f4cfb6b521852c99045ea06
Sakurako <SDT2495MV0002@edu.sca.ac.jp>
2025-11-26 14:32:57 +0900

    RankingScene背景画像　女の子表示

--- ハートキャッチゲーム/HeartCatch_project/Source/RankingScene.cpp
+++ ハートキャッチゲーム/HeartCatch_project/Source/RankingScene.cpp
@@ -2,7 +2,19 @@
 #include "DxLib.h"
 #include "../Library/SceneManager.h"
 
-RankingScene::RankingScene() {}
+int RankingbackgroundHandle;
+
+int RankingplayerHandle;
+
+RankingScene::RankingScene() 
+{
+
+    RankingbackgroundHandle = LoadGraph("data/image/ranking.png");
+
+    RankingplayerHandle = LoadGraph("data/image/woman_3.png");
+
+
+}
 RankingScene::~RankingScene() {}
 
 void RankingScene::Update()
@@ -15,20 +27,25 @@ void RankingScene::Update()
 
 void RankingScene::Draw()
 {
-    // 背景
-    DrawBox(0, 0, 1280, 720, GetColor(0, 0, 0), TRUE);
 
-    // ランキング画面の見出し
-    SetFontSize(48);
-    DrawString(400, 100, "ランキング", GetColor(255, 255, 255));
+    // 背景画像を画面全体に表示
+    DrawExtendGraph(0, 0, 1280, 720, RankingbackgroundHandle, TRUE);
 
-    // 仮のランキング表示（後でデータと連携）
-    SetFontSize(24);
-    DrawString(500, 200, "1位：さくらこ　100点", GetColor(255, 200, 200));
-    DrawString(500, 240, "2位：AIちゃん　80点", GetColor(200, 200, 255));
-    DrawString(500, 280, "3位：ねこまる　60点", GetColor(200, 255, 200));
+    //女の子を表示
+    DrawGraph(950, 60, RankingplayerHandle, TRUE);
 
-    // 戻る案内
-    SetFontSize(20);
-    DrawString(500, 600, "[Tキー] タイトルに戻る", GetColor(255, 255, 255));
+    SetFontSize(80); // フォントサイズ
+    DrawString(90, 60, "ランキング", GetColor(0, 0, 0));
+
+    SetFontSize(60); // フォントサイズ
+    DrawString(150, 220, "１位：", GetColor(0, 0, 0));
+
+    DrawString(250, 330, "２位：", GetColor(0, 0, 0));
+
+    DrawString(350, 440, "３位：", GetColor(0, 0, 0));
+    
+    DrawString(460, 550, "４位：", GetColor(0, 0, 0));
+
+
+   
 }
