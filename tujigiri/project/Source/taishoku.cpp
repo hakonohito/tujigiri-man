ae6fdb3625d89e4e9c74f82b2cc2b72d5288eb6b
so-ya <>
2025-11-26 11:54:52 +0900

    tujigiri data/image 画像追加&追加画像の表示

--- tujigiri/project/Source/taishoku.cpp
+++ tujigiri/project/Source/taishoku.cpp
@@ -8,8 +8,8 @@ Taishoku::Taishoku()
 {
 	hImage[0] = LoadGraph("data/image/Victory.png");
 	hImage[1] = LoadGraph("data/image/Lose.png");
-	hImage[2] = LoadGraph("data/image/hanko.png");
-	hImage[3] = LoadGraph("data/image/huka.png");
+	hImage[2] = LoadGraph("data/image/approval.png");
+	hImage[3] = LoadGraph("data/image/No.png");
 	angle = 10.0f * (M_PI / 180.0f);
 	SEHandle = LoadSoundMem("data/SE_BGM/playscene/‚½‚¢‚Ö‚ñ‚æ‚­‚Å‚«‚Ü‚µ‚½B.mp3");
 	assert(SEHandle != -1);
@@ -46,15 +46,15 @@ void Taishoku::Draw()
 			if (point >= 0) {
 				DrawExtendGraph(0 + 420, 0 + 35, 440 + 420, 650 + 35, hImage[1], 0);
 				if (point >= 1) {
-					DrawExtendGraph(0 + 427, 0 + 288, 115 + 427, 115 + 288, hImage[2], 0);
+					DrawExtendGraph(0 + 427, 0 + 288, 115 + 427, 115 + 288, hImage[2], 1);
 					if (point >= 2) {
-						DrawExtendGraph(0 + 583, 0 + 288, 115 + 583, 115 + 288, hImage[2], 0);
+						DrawExtendGraph(0 + 583, 0 + 288, 115 + 583, 115 + 288, hImage[2], 1);
 					}
 				}
 			}
 		}
 		if (count >= 240) {
-			DrawRotaGraph3(420 - 2, 230, 0, 0, 3.72, 1.1, angle, hImage[3], 1);
+			DrawGraph(455, 195, hImage[3], 1);
 		}
 	}
 
@@ -65,7 +65,7 @@ void Taishoku::Draw()
 			if (count >= 120) {
 				DrawExtendGraph(0 + 420, 0 + 35, 440 + 420, 650 + 35, hImage[0], 0);
 				if (count >= 240) {
-					DrawExtendGraph(0 + 445, 0 + 312, 100 + 445, 100 + 312, hImage[2], 0);
+					DrawExtendGraph(0 + 445, 0 + 312, 100 + 445, 100 + 312, hImage[2], 1);
 				}
 			}
 			break;
@@ -73,9 +73,9 @@ void Taishoku::Draw()
 		case 2:
 			if (count >= 120) {
 				DrawExtendGraph(0 + 420, 0 + 35, 440 + 420, 650 + 35, hImage[0], 0);
-				DrawExtendGraph(0 + 445, 0 + 312, 100 + 445, 100 + 312, hImage[2], 0);
+				DrawExtendGraph(0 + 445, 0 + 312, 100 + 445, 100 + 312, hImage[2], 1);
 				if (count >= 240) {
-					DrawExtendGraph(0 + 590, 0 + 312, 100 + 590, 100 + 312, hImage[2], 0);
+					DrawExtendGraph(0 + 590, 0 + 312, 100 + 590, 100 + 312, hImage[2], 1);
 				}
 			}
 			break;
@@ -83,10 +83,10 @@ void Taishoku::Draw()
 		case 3:
 			if (count >= 120) {
 				DrawExtendGraph(0 + 420, 0 + 35, 440 + 420, 650 + 35, hImage[0], 0);
-				DrawExtendGraph(0 + 445, 0 + 312, 100 + 445, 100 + 312, hImage[2], 0);
-				DrawExtendGraph(0 + 590, 0 + 312, 100 + 590, 100 + 312, hImage[2], 0);
+				DrawExtendGraph(0 + 445, 0 + 312, 100 + 445, 100 + 312, hImage[2], 1);
+				DrawExtendGraph(0 + 590, 0 + 312, 100 + 590, 100 + 312, hImage[2], 1);
 				if (count >= 240) {
-					DrawExtendGraph(0 + 735, 0 + 312, 100 + 735, 100 + 312, hImage[2], 0);
+					DrawExtendGraph(0 + 735, 0 + 312, 100 + 735, 100 + 312, hImage[2], 1);
 				}
 			}
 			break;
